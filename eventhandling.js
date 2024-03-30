7aebb4d91981097c2e9db1f4d72ee1c2c0429d6b
Santoshkumar Mishra <santoshkumarmishra@santoshmarsmini.lan>
2024-03-30 12:53:03 -0500

    some new examples

---
+++ eventhandling.js
@@ -0,0 +1,20 @@
+
+import { EventEmitter } from "events";
+
+var eventEmitter = new EventEmitter();
+
+
+
+
+//Create an event handler:
+var myEventHandler = function () {
+  console.log('I hear a scream!');
+}
+
+//Assign the event handler to an event:
+eventEmitter.on('scream', myEventHandler);
+
+
+//Fire the 'scream' event:
+eventEmitter.emit('scream');
+
---
+++ myserver.js
@@ -0,0 +1,11 @@
+
+import * as http from 'http';
+import * as fs  from 'fs';
+
+http.createServer(function (req, res) {
+  fs.readFile('demofile1.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
